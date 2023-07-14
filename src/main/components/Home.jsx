7b5bc1f6d4784bb73cc8d9cd5ad115c017b355ea
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2023-07-14 07:16:50 -0700

    added the tailwindcss to stack

---
+++ src/main/components/Home.jsx
@@ -0,0 +1,18 @@
+
+import '../assets/home.css';
+
+export default function Home() {
+  return (
+    <div className='container h-screen flex flex-col justify-center items-center place-content-center'>
+       <h1 className='className="text-3xl font-bold underline leading-10'>
+        Simple Movie DB
+       </h1>
+
+       <div className='columns-3'>
+            <div>one</div>
+            <div>two`</div>
+            <div>three</div>
+       </div>
+        </div>
+  )
+}
